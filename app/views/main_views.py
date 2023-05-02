71b3bd3dc7b7ab03c1512d95279a252397be2adb
aaa <ksh23705@naver.com>
2023-05-02 23:19:03 +0900

    add /hello homepage

--- app/views/main_views.py
+++ app/views/main_views.py
@@ -3,6 +3,10 @@ from flask import Blueprint
 bp = Blueprint('main', __name__, url_prefix='/')
 
 
-@bp.route('/')
+@bp.route('/hello')
 def hello():
     return 'Hello from python!'
+
+@bp.route('/')
+def index():
+    return 'python index'
