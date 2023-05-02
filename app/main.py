ea392e68cb71c9413370604ce3454536152ffa24
aaa <ksh23705@naver.com>
2023-05-02 22:54:24 +0900

    create bp

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from flask import Flask
-app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello from Python!"
+def create_app():
+    app = Flask(__name__)
+
+    from views import main_views
+    app.register_blueprint(main_views.bp)
+
+    return app
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
